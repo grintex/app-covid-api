6f5976972bcb01b9f25b97b6a74247fc719998b0
Fernando Bevilacqua <dovyski@gmail.com>
2020-05-18 14:18:24 -0300

    Cache stats controller results to play nice with Google Docs

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class StatsController extends Controller
 {
@@ -21,37 +22,44 @@ class StatsController extends Controller
      */
     public function index(Request $request)
     {
-        $url = config('data.spreadsheet_url');
-        $curl = curl_init($url);
+        $cacheDurationDays = 1;
+        $cacheDurationSeconds = $cacheDurationDays * 24 * 60 * 60;
 
-        $caPathOrFile = \Composer\CaBundle\CaBundle::getSystemCaRootBundlePath();
-        if (is_dir($caPathOrFile)) {
-            curl_setopt($curl, CURLOPT_CAPATH, $caPathOrFile);
-        } else {
-            curl_setopt($curl, CURLOPT_CAINFO, $caPathOrFile);
-        }
+        $items = Cache::remember('stats', $cacheDurationSeconds, function () {
+            $url = config('data.spreadsheet_url');
+            $curl = curl_init($url);
 
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-        $spreadsheet_raw_content = curl_exec($curl);
-
-        if($spreadsheet_raw_content === false) {
-            throw new \Exception('Unable to fetch data from main server.');
-        }
-        
-        $spreadsheet_lines = preg_split('/\r\n|\r|\n/', $spreadsheet_raw_content);
-        $csv = array_map('str_getcsv', $spreadsheet_lines);
-        $items = [];
-        $header = $csv[0];
-
-        foreach($csv as $index => $line) {
-            if($index == 0) {
-                // ignore header
-                continue;
+            $caPathOrFile = \Composer\CaBundle\CaBundle::getSystemCaRootBundlePath();
+            if (is_dir($caPathOrFile)) {
+                curl_setopt($curl, CURLOPT_CAPATH, $caPathOrFile);
+            } else {
+                curl_setopt($curl, CURLOPT_CAINFO, $caPathOrFile);
             }
 
-            $items[] = array_combine($header, $line);
-        }
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+            $spreadsheet_raw_content = curl_exec($curl);
+
+            if($spreadsheet_raw_content === false) {
+                throw new \Exception('Unable to fetch data from main server.');
+            }
+            
+            $spreadsheet_lines = preg_split('/\r\n|\r|\n/', $spreadsheet_raw_content);
+            $csv = array_map('str_getcsv', $spreadsheet_lines);
+            $items = [];
+            $header = $csv[0];
+
+            foreach($csv as $index => $line) {
+                if($index == 0) {
+                    // ignore header
+                    continue;
+                }
+
+                $items[] = array_combine($header, $line);
+            }
+
+            return $items;
+        });
 
         return $items;
     }
